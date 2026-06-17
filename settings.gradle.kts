31279cfc57189b17509be04880bbeb4f0462045e
kelven <tan1347@gmail.com>
2026-06-17 13:25:55 +0800

    fix build error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,11 +18,11 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        google()
-        mavenCentral()
     }
 }
 
